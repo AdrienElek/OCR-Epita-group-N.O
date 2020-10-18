5d57ac6c4e80044d56707b26cfba2eca4c3b54f0
AdrienElek <adrien.lorge@epita.fr>
2020-10-18 14:27:38 +0100

    Fichiers pour Bitmap et Segmentation

---
+++ Bitmap/Bitmap.h
@@ -0,0 +1,10 @@
+#ifndef TEST2_BITMAP_H
+#define TEST2_BITMAP_H
+
+int Get_NB_pixel(int start,const unsigned char BMPArray[]);
+
+int Fifty_Shade_of_Gray(int start,const unsigned char BMPArray[]);
+
+void Bmp_To_BArray();
+
+#endif
